545a5a1f2206e8d531d4c95ccc63fa5b3d7d73bd
Hyeon-Jun-Kim <59905688+Hyeon-Jun-Kim@users.noreply.github.com>
2022-10-02 00:04:18 +0900

    Programmers : Level 2
    이진 변환 반복하기

--- Level 2/이진 변환 반복하기/solution.playground/Contents.swift
+++ Level 2/이진 변환 반복하기/solution.playground/Contents.swift
@@ -1,17 +1,16 @@
 import UIKit
 func solution(_ s:String) -> [Int] {
+    var remove = 0
+    var roof = 0
     var s = s
-    var count = 0, times = 0
     while s != "1" {
-        let replaceCount = s.filter { $0 == "0" }.count
-        count += replaceCount
-        
-        s = String(s.count - replaceCount, radix: 2)
-        times += 1
+        roof += 1
+        remove += Array(String(s)).filter{ $0 == "0" }.count
+        s = String(String(Array(String(s)).filter{ $0 == "1" }).count, radix: 2)
     }
-    
-    return [times, count]
+    return [roof, remove]
 }
 
 
 print( solution("110010101001") )
+    
